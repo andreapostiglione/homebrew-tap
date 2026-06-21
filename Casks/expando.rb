0b3ebc19d801f3e25bd6c8d35ca6a715d1a44600
Andrea Postiglione <153174981+andreapostiglione@users.noreply.github.com>
2026-06-21 14:57:42 +0200

    chore(cask): bump expando to 3.28.0

--- Casks/expando.rb
+++ Casks/expando.rb
@@ -1,7 +1,7 @@
 cask "expando" do
-  version "3.18.0"
+  version "3.28.0"
 
-  sha256 "147733d0196be3d1b7d044be68a9b1518b166c78c816c05979dc0506b3011cbd"
+  sha256 "be5c2c6346317a7156ef50199d5e2a4bfa65e9e192de0678d32c6b9446c0d6f6"
 
   url "https://github.com/andreapostiglione/expando/releases/download/v#{version}/Expando.dmg"
   name "Expando"
