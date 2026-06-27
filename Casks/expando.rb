e110e08fb56ffe67fcb32d68514784dfbd3dacb3
Andrea Postiglione <153174981+andreapostiglione@users.noreply.github.com>
2026-06-27 13:24:08 +0200

    chore(cask): bump expando to 3.29.6 (#3)

--- Casks/expando.rb
+++ Casks/expando.rb
@@ -3,7 +3,8 @@ cask "expando" do
 
   sha256 "8079248b10a067feb0b32d4af66ccb2619102a42abe3fb71e3c78038f1f27f8e"
 
-  url "https://github.com/andreapostiglione/expando/releases/download/v#{version}/Expando.dmg"
+  url "https://github.com/andreapostiglione/expando/releases/download/v#{version}/Expando.dmg",
+      verified: "github.com/andreapostiglione/expando/"
   name "Expando"
   desc "Privacy-first open-source text expander for macOS"
   homepage "https://andreapostiglione.github.io/expando/"
