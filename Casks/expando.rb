32946dfe63e2290c486a83ceaf73100c8963e664
Andrea Postiglione <153174981+andreapostiglione@users.noreply.github.com>
2026-06-30 11:04:44 +0200

    Update expando cask to 3.29.24

--- Casks/expando.rb
+++ Casks/expando.rb
@@ -1,7 +1,7 @@
 cask "expando" do
-  version "3.29.23"
+  version "3.29.24"
 
-  sha256 "fe8371c6ab5e773924dab5d0db2f853d2eaab638aca2e78b22f747fc29b55972"
+  sha256 "4bb11a9686cb147e33df0cb594c614cf20e92b7c9c778a1559096dec995e3004"
 
   url "https://github.com/andreapostiglione/expando/releases/download/v#{version}/Expando.dmg",
       verified: "github.com/andreapostiglione/expando/"
