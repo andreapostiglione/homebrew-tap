46be6a4ef707ed9d085036bc868af2e593084a99
Andrea Postiglione <153174981+andreapostiglione@users.noreply.github.com>
2026-06-30 10:07:25 +0200

    Update expando cask to 3.29.20

--- Casks/expando.rb
+++ Casks/expando.rb
@@ -1,11 +1,10 @@
 cask "expando" do
-  version "3.29.19"
+  version "3.29.20"
 
-  sha256 "23863e2a4918f724f992f0266a8bd93896922e7eac6344df60a4b9b9198ae787"
+  sha256 "b34b08d6ce5795b70abbbf4f373eb50540c94e2eeff31a68bdbafea26bbd2ddf"
 
   url "https://github.com/andreapostiglione/expando/releases/download/v#{version}/Expando.dmg",
       verified: "github.com/andreapostiglione/expando/"
-  depends_on formula: "python@3.12"
   name "Expando"
   desc "Privacy-first open-source text expander for macOS"
   homepage "https://andreapostiglione.github.io/expando/"
